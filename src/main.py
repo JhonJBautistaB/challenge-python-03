b2e21fdd85ec07c56f2ce907acb7a7e8b89e3499
JhonJBautista <jhon.bautista@ingenieros.com>
2020-09-03 16:36:56 -0500

    [C6] Solución Reto mensaje oculto

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 # Resolve the problem!!
+import re
+import os
 
 
 def run():
     # Start coding here
-
+    regular_exp = '[a-z]'
+    ruta_actual = os.getcwd()
+    print(ruta_actual)
+    with open('src/encoded.txt', 'r', encoding='utf-8') as f:
+        resultado = re.findall(regular_exp, f.read())
+        mensaje_oculto = ''.join(resultado)
+    print(mensaje_oculto)
 
 if __name__ == '__main__':
     run()
